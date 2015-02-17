fac128a3a946b2b23fcefc9b224a047decd8d406
charroux <benoit.charroux@free.fr>
2015-02-17 16:58:22 +0100

    ajout du pattern singleton cote serveur

---
+++ src/serveur/Factory.java
@@ -0,0 +1,20 @@
+package serveur;
+
+import java.net.DatagramSocket;
+
+public class Factory{
+
+private static DatagramSocket server;
+	
+	static{
+		try{
+			server = new DatagramSocket(9876);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}	 
+
+	public static DatagramSocket getServer(){
+		return server;
+	}
+}
