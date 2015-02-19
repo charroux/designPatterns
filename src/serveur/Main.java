73ace1d22dc0619e28e081971e48d6765cb05683
charroux <benoit.charroux@free.fr>
2015-02-19 17:51:45 +0100

    utilsation factory dans le main

--- src/serveur/Main.java
+++ src/serveur/Main.java
@@ -5,19 +5,26 @@ public class Main {
 	public static void main(String[] args) {
 		try{
 			
+			Factory factory = new Factory();
+			ServerBasique serveur = factory.getServeur(false);
+			serveur.ouvrirConnection(8869);
+			String message1 = serveur.recevoir();
+			System.out.println("recu " + message1);
+			serveur.envoyer("Reponse");
+			
 /*			// sans factory UDP
 			ServerBasique serveurUDP = new ServerUDPBasique();
 			serveurUDP.ouvrirConnection(8869);
 			String message1 = serveurUDP.recevoir();
 			System.out.println("recu " + message1);
-			serveurUDP.envoyer("Reponse");*/
+			serveurUDP.envoyer("Reponse");
 			
 			// sans factory TCP
 			ServerBasique serveurTCP = new ServerTCPBasique();
 			serveurTCP.ouvrirConnection(8895);
 			String message2 = serveurTCP.recevoir();
 			System.out.println("recu " + message2);
-			serveurTCP.envoyer("Reponse");
+			serveurTCP.envoyer("Reponse");*/
 			
 		}catch(Exception e){
 			e.printStackTrace();
