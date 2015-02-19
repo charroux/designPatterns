3dfdf60f86083ccd5b17bc09b3dcfd8d45495957
charroux <benoit.charroux@free.fr>
2015-02-19 16:46:04 +0100

    Ajout d un main cote serveur

--- src/serveur/Main.java
+++ src/serveur/Main.java
@@ -5,11 +5,17 @@ public class Main {
 	public static void main(String[] args) {
 		try{
 			
-			// sans factory
-			ServerBasique serveur = new ServerUDPBasique();
-			serveur.ouvrirConnection(8869);
-			String message = serveur.recevoir();
-			serveur.envoyer("Reponse");
+			// sans factory UDP
+			ServerBasique serveurUDP = new ServerUDPBasique();
+			serveurUDP.ouvrirConnection(8869);
+			String message1 = serveurUDP.recevoir();
+			serveurUDP.envoyer("Reponse");
+			
+			// sans factory TCP
+			ServerBasique serveurTCP = new ServerUDPBasique();
+			serveurTCP.ouvrirConnection(8869);
+			String message2 = serveurTCP.recevoir();
+			serveurTCP.envoyer("Reponse");
 			
 		}catch(Exception e){
 			e.printStackTrace();
